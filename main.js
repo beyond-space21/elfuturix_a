bb3433fdcf65bd2e41b0ba310b2a3cedb4e24784
rajanand <ultimatzanand@gmail.com>
2024-10-12 15:11:59 +0530

    sw

--- main.js
+++ main.js
@@ -16,3 +16,28 @@ get_details().then((userDetails) => {
   }).catch((error) => {
     console.error("Error getting user details:", error);
   });
+
+
+  document.getElementsByClassName("profile")[0].onclick = ()=>{
+    alert(countdown())
+  }
+
+  function countdown(){
+    const countDate = new Date("October 12, 2024 17:00:00").getTime();
+
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    // Time calculations for days, hours, minutes, and seconds
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const days = Math.floor(gap / day);
+    const hours = Math.floor((gap % day) / hour);
+    const minutes = Math.floor((gap % hour) / minute);
+    const seconds = Math.floor((gap % minute) / second);
+
+    return "Event launches in " + hours + "Hours " + minutes + "Minutes " + seconds + "Seconds"
+};
